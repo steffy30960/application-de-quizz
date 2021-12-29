63efde89bc23403485242c4391420b5943885a7c
stephanie <stephanie.menu.cda@gmail.com>
2021-12-29 11:32:03 +0100

    modif

---
+++ quizz.js
@@ -0,0 +1,24 @@
+ 
+start();
+async function start()
+{
+   const data = await getJson(); // Récupère le tableau json
+   
+   // Affiche pour tester la première question dans la console
+   console.log(data)
+}
+//je crée une fonction pour recuperer mon fichier json qui contient le quizz//
+async function getJson()
+{
+    // Récupère les données avec la fonction fetch()
+    // Ici le fichier s'appelle quiz.json et il est situé à la racine "/" du dossier projet
+    const data = await fetch("http://localhost:5500//quizz.json");
+  
+    return data.json(); // Retourne les données au format Json
+ }
+// Fonction de MÉLANGE ALÉATOIRE d’un tableau
+function shuffleArray(array) {
+    return array.sort(() => 0.5 - Math.random());
+ }
+// je recupere la question 1//
+const question1Element = document.getElementById("question1");
